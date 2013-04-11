6cafc08885c1c7cc6421834887dd1d63647585be
bumblehead <chrisdep@FNG117021.local>
2013-04-10 19:16:50 -0700

    updated unit test -removed console.log

--- test/worldtime.spec.js
+++ test/worldtime.spec.js
@@ -3,14 +3,11 @@ var WorldTime = require('../worldtime');
 describe("WorldTime.isTest", function () {
 
   it("should return true", function () {
-
-    console.log(WorldTime('EN-us'));
     expect(WorldTime('EN-us').isTest()).toBe(true);
   });
 
   it("should load a locale map", function (done) {
     WorldTime('EN-us').localeMethods.loadMap('../locale/en-US/base.json', function (err, res) {
-      consoe.log('base.json?');
       expect( err ).toBe( null );      
       done();
     });
